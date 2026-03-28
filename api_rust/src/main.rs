712cd93d757d0f7ee5d71f8f58b11928c039e354
Amenhotep314 <nathan.lenzini@gmail.com>
2026-03-27 20:12:30 -0400

    Adds .env configuration

--- api_rust/src/main.rs
+++ api_rust/src/main.rs
@@ -2,17 +2,26 @@ mod models;
 mod routes_auth;
 
 use axum::{routing::get, Router};
+use std::env;
 use migration::{Migrator, MigratorTrait};
 
 
-let connection = sea_orm::Database::connect(&database_url).await?;
-Migrator::up(&connection, None).await?;
-
 #[tokio::main]
 async fn main() {
+    // Load environment variables
+    dotenv::dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set in .env.");
+    let app_host = env::var("APP_HOST").expect("APP_HOST must be set in .env.");
+    let app_port = env::var("APP_PORT").expect("APP_PORT must be set in .env.");
+
+    // Launch app
+    let addr = format!("{}:{}", &app_host, &app_port);
     let app = Router::new().merge(api_routes());
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:5000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
+
+    let connection = sea_orm::Database::connect(&database_url).await.unwrap();
+    Migrator::up(&connection, None).await.unwrap();
 }
 
 fn api_routes() -> Router {
